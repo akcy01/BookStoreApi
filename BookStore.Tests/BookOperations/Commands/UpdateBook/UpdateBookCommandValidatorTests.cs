4936da1d2221f1308018cc0980f2329f198db51e
akcy01 <98681852+akcy01@users.noreply.github.com>
2022-10-08 13:36:19 +0300

    UpdateBookCommandValidatorTests has been passed

---
+++ BookStore.Tests/BookOperations/Commands/UpdateBook/UpdateBookCommandValidatorTests.cs
@@ -0,0 +1,54 @@
+﻿using BookStore.BookOperations.UpdateBooks;
+using BookStore.Tests.TestsSetup;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Tests.BookOperations.Commands.UpdateBook
+{
+    public class UpdateBookCommandValidatorTests : IClassFixture<CommonTestFixture>
+    {   
+        [Theory]
+        [InlineData("Lord Of The Rings", 0, 0)]
+        [InlineData("Lord Of The Rings", 0, 1)]
+        [InlineData("Lord Of The Rings", 100, 0)]
+        [InlineData("", 0, 0)]
+        [InlineData("", 100, 1)]
+        [InlineData("", 0, 1)]
+        [InlineData(" ", 100, 1)]
+        public void WhenInvalidInputsAreGiven_Validator_ShouldBeReturnError(string title,int genreId,int bookId)
+        {
+            UpdateBookCommand command = new UpdateBookCommand(null);
+            command.BookId = bookId;
+            command.Model = new UpdateBookModel()
+            {
+                Title = title,
+                GenreId = genreId,
+            };
+
+            UpdateBookCommandValidator validator = new UpdateBookCommandValidator();
+            var result = validator.Validate(command);
+
+            result.Errors.Count.Should().BeGreaterThan(0);
+        }
+        [Fact]
+        public void WhenValidInputsAreGiven_Validator_ShouldNotBeReturnError()
+        {
+            UpdateBookCommand command = new UpdateBookCommand(null);
+            command.BookId = 1;
+            command.Model = new UpdateBookModel()
+            {
+                Title = "DenemeTitleDeneme",
+                GenreId = 1,
+            };
+
+            UpdateBookCommandValidator validator = new UpdateBookCommandValidator();
+            var result = validator.Validate(command);
+
+            result.Errors.Count().Should().Be(0);
+        }
+    }
+}
