8aeba9b1968fd8c831b40f37e4f8d2897fa9b94d
akcy01 <98681852+akcy01@users.noreply.github.com>
2022-10-13 17:59:25 +0300

    ValidatorTest has been added.

---
+++ BookStore.Tests/GenreOperations/Commands/CreateGenre/CreateGenreCommandValidatorTests.cs
@@ -0,0 +1,38 @@
+﻿using BookStore.GenreOperations.CreateGenre;
+using BookStore.Tests.TestsSetup;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Tests.GenreOperations.Commands.CreateGenre
+{
+    public class CreateGenreCommandValidatorTests : IClassFixture<CommonTestFixture>
+    {
+        [Theory]
+        [InlineData("",false)]
+        public void WhenInvalidInputAreGiven_Validator_ShouldBeReturnErrors(string name,bool IsActive)
+        {
+            CreateGenreCommand command = new CreateGenreCommand(null);
+            command.Model = new CreateGenreModel() { Name = name };
+
+            CreateGenreCommandValidator validator = new CreateGenreCommandValidator();
+            var result = validator.Validate(command);
+
+            result.Errors.Count.Should().BeGreaterThan(0);
+        }
+        [Fact]
+        public void WhenValidInputsAreGiven_Validator_ShouldNotBeReturnError()
+        {
+            CreateGenreCommand command = new CreateGenreCommand(null);
+            command.Model = new CreateGenreModel() { Name = "DenemeAkcay" };
+
+            CreateGenreCommandValidator validator = new CreateGenreCommandValidator();
+            var result = validator.Validate(command);
+
+            result.Errors.Count.Should().Be(0);
+        }
+    }
+}
