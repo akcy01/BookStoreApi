021edd8541e4876b49c9c5aa99f38303301ceed4
akcy01 <98681852+akcy01@users.noreply.github.com>
2022-10-13 21:31:50 +0300

    DeleteGenreTests has been completed.

---
+++ BookStore.Tests/GenreOperations/Commands/DeleteGenre/DeleteGenreCommandTests.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using BookStore.Data;
+using BookStore.GenreOperations.DeleteGenre;
+using BookStore.Tests.TestsSetup;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Tests.GenreOperations.Commands.DeleteGenre
+{
+    public class DeleteGenreCommandTests : IClassFixture<CommonTestFixture>
+    {
+        private readonly ApplicationDbContext __context;
+        public DeleteGenreCommandTests(CommonTestFixture testFixture)
+        {
+            __context = testFixture._context;
+        }
+        [Fact]
+        public void WhenGenreIdIsNotFound_InvalidOperationExceptionShouldBeReturn()
+        {
+            DeleteGenreCommand command = new DeleteGenreCommand(__context);
+            command.GenreId = 999;
+
+            FluentActions.Invoking(() => command.Handle()).Should().Throw<InvalidOperationException>().And.Message.Should().Be("The book has not found");
+        }
+    }
+}
---
+++ BookStore.Tests/GenreOperations/Commands/DeleteGenre/DeleteGenreCommandValidatorTests.cs
@@ -0,0 +1,33 @@
+﻿using BookStore.BookOperations.DeleteBook;
+using BookStore.Data;
+using BookStore.GenreOperations.DeleteGenre;
+using BookStore.Tests.TestsSetup;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Tests.GenreOperations.Commands.DeleteGenre
+{
+    public class DeleteGenreCommandValidatorTests : IClassFixture<CommonTestFixture>
+    {
+        private readonly ApplicationDbContext __context;
+        public DeleteGenreCommandValidatorTests(CommonTestFixture testFixture)
+        {
+            __context = testFixture._context;
+        }
+        [Fact]
+        public void WhenInvalidInputIsGiven_Validator_ShouldBeReturnError()
+        {
+            DeleteGenreCommand command = new DeleteGenreCommand(__context);
+            command.GenreId = -3;
+
+            DeleteGenreCommandValidator validator = new DeleteGenreCommandValidator();
+            var result = validator.Validate(command);
+
+            result.Errors.Count.Should().BeGreaterThan(0);
+        }
+    }
+}
